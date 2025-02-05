bb292fe479ab8652bf41526a2a7487ebf59e75ab
Masalale <clarenchomba59@gmail.com>
2025-02-05 11:13:05 +0000

    Commenting the code

--- analyzed_data/highest_temp.sh
+++ analyzed_data/highest_temp.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+
+# Extract data for Rwanda from the satelite_temperature_data.csv file
+# Sort the data by temperature in descending order
+# Save the extracted data in a new file called highest_temp.csv
 (head -n 1 ../raw_data/satelite_temperature_data.csv; tail -n +2 ../raw_data/satelite_temperature_data.csv | sort -t',' -k2 -nr | head -n 10) > highest_temp.csv
--- analyzed_data/humidity_data.sh
+++ analyzed_data/humidity_data.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# (head -n 1 ../raw_data/satelite_temperature_data.csv; grep "Rwanda" ../raw_da"/satelite_temperature_data.csv | sort -t',' -k3 -nr) > "humidity_data_rwanda.csv"
-
+# Extract data for Rwanda from the satelite_temperature_data.csv file
+# Sort the data by humidity in descending order
+# Save the extracted data in a new file called humidity_data_Rwanda.csv
 (head -n 1 ../raw_data/satelite_temperature_data.csv; grep "Rwanda" ../raw_data/satelite_temperature_data.csv |
  sort -t',' $(head -n 1 ../raw_data/satelite_temperature_data.csv | tr ',' '\n' | grep -n "Humidity" | cut -d':' -f1 |
  sed 's/^/-k/') -nr) > humidity_data_Rwanda.csv
